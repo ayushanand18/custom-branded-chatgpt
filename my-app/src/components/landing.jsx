69e4bd276f95219b2343b76a6ba3781df20b4490
Ayush Anand <36472216+ayushanand18@users.noreply.github.com>
2023-05-16 13:41:35 +0000

    feat(landing): completed styling of the landing page

--- my-app/src/components/landing.jsx
+++ my-app/src/components/landing.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import '../styles/landing.module.css';
+import '../styles/landing.css';
+import Logo from '../assets/logo.svg';
 
 function LandingApp() {
     return (
         <div className="flexHome">
             <div className="flexMain">
-                <div className="">
-                    <Logo />
+                <div className="logoContainer">
+                    <img src={Logo} className="appLogo" alt="logo" />
                 </div>
                 <div className="welcomeText">
                     Welcome to ChatGPT
@@ -18,6 +19,11 @@ function LandingApp() {
                     <button className="loginButton">Log in</button>
                     <button className="signupButton">Sign up</button>
                 </div>
+            </div>
+            <div className="footer">
+                <a href="#terms-of-use" target="_blank" rel="noreferrer">Terms of use</a>
+                <span className="text-gray-600">|</span>
+                <a href="#privacy-policy" target="_blank" rel="noreferrer">Privacy policy</a>
             </div>
         </div>
     )
