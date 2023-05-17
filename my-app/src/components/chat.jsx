abc09ba2bd1726efd5dfc08e63197828df5d0756
Ayush Anand <36472216+ayushanand18@users.noreply.github.com>
2023-05-17 17:45:48 +0000

    adding navbar for mobile

--- my-app/src/components/chat.jsx
+++ my-app/src/components/chat.jsx
@@ -62,6 +62,23 @@ function Chat(){
         <div className="wrapper">
             <div className="overlay"></div>
             <div className="container">
+                <div class="stickyTopbar">
+                    <button type="button" class="inlineFlex">
+                        <span class="sr-only">Open sidebar</span>
+                        <svg stroke="currentColor" fill="none" stroke-width="1.5" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                            <line x1="3" y1="12" x2="21" y2="12"></line>
+                            <line x1="3" y1="6" x2="21" y2="6"></line>
+                            <line x1="3" y1="18" x2="21" y2="18"></line>
+                        </svg>
+                    </button>
+                    <h1 class="flexTextNormal">ChatGPT</h1>
+                    <button type="button" class="px-3">
+                        <svg stroke="currentColor" fill="none" stroke-width="1.5" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                            <line x1="12" y1="5" x2="12" y2="19"></line>
+                            <line x1="5" y1="12" x2="19" y2="12"></line>
+                        </svg>
+                    </button>
+                </div>
                 <div className="navSection">
                     <div className="flexDiv">
                         <div className="navMenu">
@@ -111,7 +128,7 @@ function Chat(){
                             </span>
                         </nav>
                     </div>
-                    
+
                     <div className="border-t">
                         <span className="py-3">
                             <span className="w-full" onClick={handleOpenContext}>
