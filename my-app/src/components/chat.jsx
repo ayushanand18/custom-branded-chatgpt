9092327df15714a5e77b0eb1c80c7fe3e9de3203
Ayush Anand <36472216+ayushanand18@users.noreply.github.com>
2023-05-23 18:17:39 +0000

    feat(icon): updte rename and delete icon

--- my-app/src/components/chat.jsx
+++ my-app/src/components/chat.jsx
@@ -270,6 +270,10 @@ function Chat(){
         setMessageCount(messageCount+1)
     }
 
+    function handleDeleteChat(){
+
+    }
+
     function handleOpenDialog() {
         const newVisibility = dialogVisibility^true;
         setDialogVisbility(newVisibility);
@@ -492,8 +496,19 @@ function Chat(){
                                     <span className="span" ref={currentDoc}>
                                         {defaultDoc?.name}
                                     </span>
-                                    <span style={{display:contentEditable?"none":"flex"}} className="rounded-md" onClick={(event)=>handleChatPencil(event)}>
-                                        RENAME
+                                    <span style={{display:contentEditable?"none":"flex"}} onClick={(event)=>handleChatPencil(event)}>
+                                        <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="1.4em" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M12 20h9"></path>
+                                            <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
+                                        </svg>
+                                    </span>
+                                    <span style={{backgroundColor:"transparent"}} onClick={handleDeleteChat} >
+                                        <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="1.4em" xmlns="http://www.w3.org/2000/svg">
+                                            <polyline points="3 6 5 6 21 6"></polyline>
+                                            <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                                            <line x1="10" y1="11" x2="10" y2="17"></line>
+                                            <line x1="14" y1="11" x2="14" y2="17"></line>
+                                        </svg>
                                     </span>
                                     <span style={{display:contentEditable?"flex":"none", backgroundColor:"#43e500"}} className="rounded-md" onClick={(event)=>handleChatRename(event)}>
                                         DONE
