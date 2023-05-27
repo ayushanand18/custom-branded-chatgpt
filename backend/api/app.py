c3bf0203db4815eb664cf9821f3f6d40b5450aec
Ayush Anand <36472216+ayushanand18@users.noreply.github.com>
2023-05-27 10:29:05 +0000

    feat(backend): setting up backend

---
+++ backend/api/app.py
@@ -0,0 +1,18 @@
+"""
+API for 
+"""
+import asyncio
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+#--------------------
+# Application routes
+#--------------------
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
